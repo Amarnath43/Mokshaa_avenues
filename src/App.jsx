ea9dedcaea9c0873584848b065196ba19c3e3fae
Amarnath43 <amarnathgoshika@gmail.com>
2025-04-29 17:01:46 +0530

    Live code version 1.0 updated logo

--- src/App.jsx
+++ src/App.jsx
@@ -49,7 +49,7 @@ function App() {
       <div ref={locationMapRef}><LocationMap ></LocationMap></div>
       
       <Footer/>
-      <div className="text-center bg-amber-300 font-bold text-black px-4 py-2 sm:hidden w-full fixed bottom-0">
+      <div className="text-center bg-amber-300 font-bold text-black px-4 py-2 lg:hidden w-full fixed bottom-0">
       <DownloadBrochure/>
       </div>
     </div>
